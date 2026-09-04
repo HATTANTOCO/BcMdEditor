55078faa00f4ebd76d5f9911e708bac53fa40edb
HATTANTOCO <hattantoco@gmail.com>
2026-09-05 00:29:30 +0900

    プラグイン無効化時のセキュリティトークンエラー（BadRequestException）を修正

--- src/Event/BcMdEditorControllerEventListener.php
+++ src/Event/BcMdEditorControllerEventListener.php
@@ -50,68 +50,68 @@ class BcMdEditorControllerEventListener extends BcControllerEventListener
     {
         /** @var \Cake\Controller\Controller $controller */
         $controller = $event->getSubject();
-        
-        // 対象外のコントローラー、または POST/PUT でなければ即座に終了する
-        $targetControllers = ['Pages', 'PagesAdmin', 'BlogPosts'];
-        if (!in_array($controller->getName(), $targetControllers)) {
+        if (!in_array($controller->getName(), ['Pages', 'PagesAdmin', 'BlogPosts'])) {
             return;
         }
-
         $request = $controller->getRequest();
-        if (!$request->is(['post', 'put'])) {
-            return;
-        }
 
         // 対象コントローラーに対するForm保護解除
-        if ($controller->components()->has('FormProtection')) {
-            /** @var \Cake\Controller\Component\FormProtectionComponent $formProtection */
-            $formProtection = $controller->components()->get('FormProtection');
-            $unlockedFields = $formProtection->getConfig('unlockedFields') ?? [];
-            
-            // 既存 of フィールドロック解除処理
-            foreach (['contents', 'detail', 'contents_tmp', 'Page.contents', 'BlogPost.detail'] as $field) {
-                if (!in_array($field, $unlockedFields)) { $unlockedFields[] = $field; }
-            }
-            $formProtection->setConfig('unlockedFields', $unlockedFields);
+        $targetControllers = ['Pages', 'PagesAdmin', 'BlogPosts'];
+        if (in_array($controller->getName(), $targetControllers)) {
+            if ($request->is(['post', 'put'])) {
+                if ($controller->components()->has('FormProtection')) {
+                    /** @var \Cake\Controller\Component\FormProtectionComponent $formProtection */
+                    $formProtection = $controller->components()->get('FormProtection');
+                    $unlockedFields = $formProtection->getConfig('unlockedFields') ?? [];
+                    
+                    // 既存のフィールドロック解除処理
+                    foreach (['contents', 'detail', 'contents_tmp', 'Page.contents', 'BlogPost.detail'] as $field) {
+                        if (!in_array($field, $unlockedFields)) { $unlockedFields[] = $field; }
+                    }
+                    $formProtection->setConfig('unlockedFields', $unlockedFields);
 
-            // 画像アップロード用のアクションをFormProtectionの監視対象から除外
-            $unlockedActions = $formProtection->getConfig('unlockedActions') ?? [];
-            if (!in_array('upload', $unlockedActions)) {
-                $unlockedActions[] = 'upload';
+                    // 画像アップロード用のアクションをFormProtectionの監視対象から除外
+                    $unlockedActions = $formProtection->getConfig('unlockedActions') ?? [];
+                    if (!in_array('upload', $unlockedActions)) {
+                        $unlockedActions[] = 'upload';
+                    }
+                    $formProtection->setConfig('unlockedActions', $unlockedActions);
+                }
             }
-            $formProtection->setConfig('unlockedActions', $unlockedActions);
         }
 
         // リクエストデータを読み込み、値をセットしてコントローラーに書き戻す処理
-        $parsedBody = $request->getParsedBody();
-        $isChanged = false;
+        if ($request->is(array('post', 'put'))) {
+            $parsedBody = $request->getParsedBody();
+            $isChanged = false;
 
-        // 固定ページデータの処理
-        if (isset($parsedBody['Page']['contents']) || isset($parsedBody['contents'])) {
-            $rawText = isset($parsedBody['Page']['contents']) ? (string)$parsedBody['Page']['contents'] : (string)$parsedBody['contents'];
-            if ($rawText !== '') {
-                if (isset($parsedBody['Page'])) {
-                    $parsedBody['Page']['contents'] = $rawText;
-                    $parsedBody['Page']['contents_tmp'] = $rawText;
+            // 固定ページデータの処理
+            if (isset($parsedBody['Page']['contents']) || isset($parsedBody['contents'])) {
+                $rawText = isset($parsedBody['Page']['contents']) ? (string)$parsedBody['Page']['contents'] : (string)$parsedBody['contents'];
+                if ($rawText !== '') {
+                    if (isset($parsedBody['Page'])) {
+                        $parsedBody['Page']['contents'] = $rawText;
+                        $parsedBody['Page']['contents_tmp'] = $rawText;
+                    }
+                    $parsedBody['contents'] = $rawText;
+                    $parsedBody['contents_tmp'] = $rawText;
+                    $isChanged = true;
                 }
-                $parsedBody['contents'] = $rawText;
-                $parsedBody['contents_tmp'] = $rawText;
-                $isChanged = true;
             }
-        }
 
-        // ブログ記事データの処理
-        if (isset($parsedBody['BlogPost']['detail']) || isset($parsedBody['detail'])) {
-            $rawText = isset($parsedBody['BlogPost']['detail']) ? (string)$parsedBody['BlogPost']['detail'] : (string)$parsedBody['detail'];
-            if ($rawText !== '') {
-                if (isset($parsedBody['BlogPost'])) { $parsedBody['BlogPost']['detail'] = $rawText; }
-                $parsedBody['detail'] = $rawText;
-                $isChanged = true;
+            // ブログ記事データの処理
+            if (isset($parsedBody['BlogPost']['detail']) || isset($parsedBody['detail'])) {
+                $rawText = isset($parsedBody['BlogPost']['detail']) ? (string)$parsedBody['BlogPost']['detail'] : (string)$parsedBody['detail'];
+                if ($rawText !== '') {
+                    if (isset($parsedBody['BlogPost'])) { $parsedBody['BlogPost']['detail'] = $rawText; }
+                    $parsedBody['detail'] = $rawText;
+                    $isChanged = true;
+                }
             }
-        }
 
-        if ($isChanged) {
-            $controller->setRequest($request->withParsedBody($parsedBody));
+            if ($isChanged) {
+                $controller->setRequest($request->withParsedBody($parsedBody));
+            }
         }
     }
 }
